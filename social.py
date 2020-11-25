d4c72d80f52abd8418e8af80fa16536d70968ed8
rivera-fernando <f.rivera@ufl.edu>
2020-11-25 18:15:02 -0500

    formatting info

--- social.py
+++ social.py
@@ -20,7 +20,7 @@ def show_info():
     if phone == "null":
         st.markdown("Phone: **not provided**")
     else:
-        st.markdown("Phone: ["+phone+"]("+ phone +")")
+        st.markdown('<a href="tel:123-456-7890">123-456-7890</a>', True)
     st.text(query_params['email'][0])
 
     st.header("Socials")
